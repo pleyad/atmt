4ae5bbfb009691b903d3b4876ebf2b5c8de3265a
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-11-15 17:00:22 +0100

    Add model descriptions

--- data/en/filter.py
+++ data/en/filter.py
@@ -1,16 +1,16 @@
 import sys
 
 for line in sys.stdin:
-    if "É" in line:
+    if "É" in line:  # Probably an encoding artifact
         continue
     if line.startswith("- "):
         line = line[2:]
     elif line.startswith("-"):
         line = line[1:]
-    if any(bracket in line for bracket in "()[]"):
+    if any(bracket in line for bracket in "()[]"):  # Non-speech comments
         continue
-    if len(line) < 20:
+    if len(line) < 20:  # Too short
         continue
-    if sum(character.isascii() for character in line) / len(line) < 0.2:
+    if sum(character.isascii() for character in line) / len(line) < 0.2:  # Noise
         continue
     print(line, end="")
